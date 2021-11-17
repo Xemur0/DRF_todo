74ebe1d3d79e57e5c0cfca71e1abceecf335566b
Xemur0 <aleks.prfnk@gmail.com>
2021-11-17 16:04:10 +0300

    first commit

---
+++ users/management/commands/fill_users.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from users.models import User
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+
+        super_user = User.objects.create_superuser('Xemur0', '', '1')
+        user1 = User.objects.create_user('user1', 'user1@mail.ru','1')
+        user2 = User.objects.create_user('user2', 'user2@mail.ru','2')
