f70deac5eb6e5b78b573fd8f7af59a78e7610964
Xemur0 <aleks.prfnk@gmail.com>
2021-11-29 17:49:40 +0300

    override destroy method for TodoModelViewSet

--- project/views.py
+++ project/views.py
@@ -1,13 +1,17 @@
+from rest_framework import status
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from .filters import ProjectFilter, ToDoFilter
 from .models import Project, Todo
 from .serializers import ProjectModelSerializer, TodoModelSerializer
 
+
 class LimitListForProject(LimitOffsetPagination):
-   default_limit = 10
+    default_limit = 10
+
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
@@ -18,7 +22,8 @@ class ProjectModelViewSet(ModelViewSet):
 
 
 class LimitListForTodo(LimitOffsetPagination):
-   default_limit = 20
+    default_limit = 20
+
 
 class TodoModelViewSet(ModelViewSet):
     queryset = Todo.objects.all()
@@ -26,7 +31,8 @@ class TodoModelViewSet(ModelViewSet):
     pagination_class = LimitListForTodo
     filterset_class = ToDoFilter
 
-
-
-
-
+    def destroy(self, request, *args, **kwargs):
+        todo = self.get_object()
+        todo.is_active = False
+        todo.save()
+        return Response(status=status.HTTP_200_OK)
