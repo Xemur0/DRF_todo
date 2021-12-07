5673fab754da1b64934a91a27a511df0b318abfe
Xemur0 <aleks.prfnk@gmail.com>
2021-12-07 21:36:13 +0300

    login/logout, show username who logged, add cookies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,14 @@ import axios from "axios";
 import UserList from "./components/User";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import {Route, BrowserRouter} from "react-router-dom";
+import {Route, BrowserRouter, Link} from "react-router-dom";
 import ProjectList from "./components/Projects";
 import ToDoList from "./components/ToDo";
+import LoginForm from "./components/LoginForm";
+import Cookies from "universal-cookie/lib";
+import {Button} from "reactstrap";
+
+
 
 
 class App extends React.Component {
@@ -16,11 +21,59 @@ class App extends React.Component {
             'users': [],
             'projects': [],
             'todos': [],
+            'token': '',
+            'username': ''
         }
     }
 
-    componentDidMount() {
-        axios.get('http://127.0.0.1:8000/api/users/').then(
+    set_token(token) {
+        console.log(token)
+        const cookies = new Cookies()
+        cookies.set('token', token)
+        this.setState({'token': token}, () => this.load_data())
+    }
+    set_username(username) {
+        const cookies = new Cookies()
+        cookies.set('username', username)
+        this.setState({'username': username})
+    }
+
+    is_auth() {
+        return !!this.state.token
+    }
+
+    logout() {
+        this.set_token('')
+    }
+
+    get_token_from_storage() {
+
+        const cookies = new Cookies()
+        const token = cookies.get('token')
+        const username = cookies.get('username')
+        this.setState({'token': token}, () => this.load_data())
+        this.setState({'username': username})
+    }
+
+
+    get_token(username, password) {
+        const data = {username: username, password: password}
+        axios.post('http://127.0.0.1:8000/api-token-auth/', data).then(
+            response => {
+                this.set_token(response.data['token'])
+                this.set_username(username)
+
+                console.log(response.data)
+                // cookies.set('token', response.data)
+            }
+        ).catch(error => alert('Wrong login or password'))
+
+    }
+
+    load_data() {
+        const headers = this.get_headers()
+
+        axios.get('http://127.0.0.1:8000/api/users/', {headers}).then(
             response => {
                 const users = response.data
                 this.setState(
@@ -29,10 +82,13 @@ class App extends React.Component {
                     }
                 )
             }
-        ).catch(error => console.log(error))
+        ).catch(error => {
+            console.log(error)
+            this.setState({users: []})
+        })
 
 
-        axios.get('http://127.0.0.1:8000/api/project/').then(
+        axios.get('http://127.0.0.1:8000/api/project/', {headers}).then(
             response => {
                 const projects = response.data.results
                 this.setState(
@@ -41,9 +97,12 @@ class App extends React.Component {
                     }
                 )
             }
-        ).catch(error => console.log(error))
+        ).catch(error => {
+            console.log(error)
+            this.setState({projects: []})
+        })
 
-        axios.get('http://127.0.0.1:8000/api/todo/').then(
+        axios.get('http://127.0.0.1:8000/api/todo/', {headers}).then(
             response => {
                 const todos = response.data.results
                 this.setState(
@@ -52,7 +111,24 @@ class App extends React.Component {
                     }
                 )
             }
-        ).catch(error => console.log(error))
+        ).catch(error => {
+            console.log(error)
+            this.setState({todos: []})
+        })
+    }
+
+    get_headers() {
+        let headers = {
+            'Content-Type': 'application/json'
+        }
+        if (this.is_auth()) {
+            headers['Authorization'] = 'Token ' + this.state.token
+        }
+        return headers
+    }
+
+    componentDidMount() {
+        this.get_token_from_storage()
     }
 
     render() {
@@ -64,7 +140,23 @@ class App extends React.Component {
                     <Route exact path='/users' component={() => <UserList users={this.state.users}/>}/>
                     <Route exact path='/project' component={() => <ProjectList projects={this.state.projects}/>}/>
                     <Route exact path='/todo' component={() => <ToDoList todos={this.state.todos}/>}/>
+                    <Route exact path='/login' component={() => <LoginForm
+                        get_token={(username, password) => this.get_token(username, password)}/>}/>
 
+
+
+                    {/*<Navbar>*/}
+                    <div className={'button_login'}>
+                        <Button>
+                            {this.is_auth() ? <div> {this.state.username + ' '}
+                                <Button onClick={() => this.logout()}> Выйти
+                            </Button> </div> :
+                                <Link to='/login'> Войти</Link>}
+                            </Button>
+                    </div>
+
+
+                    {/*</Navbar>*/}
                 </BrowserRouter>
                 <Footer/>
             </div>
